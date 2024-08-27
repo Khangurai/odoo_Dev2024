4a8f293ed46789bee9cbe699573140c8efc4575e
Khangurai <glorykai7@gmail.com>
2024-08-27 14:11:18 +0630

    [ADD] : odoo15 fix error

--- custom_module/hotel_b_inheritence/models/sale_order.py
+++ custom_module/hotel_b_inheritence/models/sale_order.py
@@ -1,28 +1,28 @@
 from odoo import api, fields, models
 
-# class SaleOrder(models.Model):
-#     _inherit = 'sale.order'
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
 
-
-#     name = fields.Char(string='Name', required=True)
-
-#     confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
-
-#     def action_confirm(self):
-#         print("Success......................")
-#         super(SaleOrder, self).action_confirm()
-#         self.confirmed_user_id = self.env.user.id
-
-class SaleOrderInherit(models.Model):
-    _name = 'sale.order.inherit'
-    _inherit = []
 
     name = fields.Char(string='Name', required=True)
 
     confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
 
-class SaleOrderDelegate(models.Model):
-    _name = 'sale.order.delegate'
-    _inherits = {'estate.property': 'estate_id'}
+    def action_confirm(self):
+        print("Success......................")
+        super(SaleOrder, self).action_confirm()
+        self.confirmed_user_id = self.env.user.id
 
-    estate_id = fields.Many2one(comodel_name='estate.property')
+# class SaleOrderInherit(models.Model):
+#     _name = 'sale.order.inherit'
+#     _inherit = []
+#
+#     name = fields.Char(string='Name', required=True)
+#
+#     confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
+#
+# class SaleOrderDelegate(models.Model):
+#     _name = 'sale.order.delegate'
+#     _inherits = {'estate.property': 'estate_id'}
+#
+#     estate_id = fields.Many2one(comodel_name='estate.property')
